c9b48743af40de7cdf1b0f0215d0fade9f1ca204
ostadalv-tmg <vojtech.ostadal@telegraph.co.uk>
2026-02-02 19:02:13 +0100

    update db

--- scripts/db_init.ts
+++ scripts/db_init.ts
@@ -15,3 +15,27 @@ db.run(`
       colors TEXT CHECK(json_valid(colors))
   );
 `);
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS matches (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      slug TEXT NOT NULL UNIQUE,
+      home_team_id TEXT NOT NULL,
+      away_team_id TEXT NOT NULL,
+      date TEXT
+  );
+`);
+
+// timestamp shuld be play time
+// date is just when it was created
+// only team is required, the idea is to update the entry with more information
+db.run(`
+  CREATE TABLE IF NOT EXISTS events (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      team TEXT NOT NULL,
+      player TEXT,
+      type TEXT,
+      timestamp TEXT,
+      date TEXT
+  );
+`);
